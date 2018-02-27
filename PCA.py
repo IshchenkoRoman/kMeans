fc185e8a9af570875d8d7f41b62f725b53f7d6f6
rishchen <rishchen@localhost.localdomain>
2018-02-27 21:55:23 +0000

    Part 2

--- PCA.py
+++ PCA.py
@@ -5,9 +5,14 @@ import operator
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib import colors
+from mpl_toolkits.mplot3d import Axes3D
 
 from scipy.io import loadmat
 from scipy.optimize import minimize
+
+import kMeans as kM
 
 class PCA():
 
@@ -141,27 +146,53 @@ class PCA():
 		ax.set_ylim(-4, 3)
 		plt.show()
 
-	def plotFaces(self, X):
+	def plotFaces(self, X, count=10, title="First N faces"):
 
 		fig = plt.figure()
+		m, n = X.shape
 
-		for i in range(1, 11):
-			fig.add_subplot(10, 1, i)
-			array = X[(i - 1) * 10:(i) * 10].reshape(-1, 32).T
-			plt.imshow(array, cmap="gray")
+		# Alternative
+
+		# gs = gridspec.GridSpec(count, count)
+		# gs.update(bottom=0.01, top=0.99, left=0.01, right=0.99, hspace=0.05, wspace=0.05)
+		# k = 0
+		# for i in range(count):
+		# 	for j in range(count):
+		# 		ax = plt.subplot(gs[i, j])
+		# 		ax.axis('off')
+		# 		ax.imshow(X[i + j].reshape(int(np.sqrt(n)), int(np.sqrt(n))).T, cmap=plt.get_cmap("Greys"), interpolation = "nearest")
+
+		for i in range(1, count + 1):
+			fig.add_subplot(count, 1, i)
+			array = X[(i - 1) * count:(i) * count].reshape(-1, int(np.sqrt(n))).T
+			plt.imshow(array, cmap="gray", interpolation="nearest")
 			plt.axis("off")
 
-		plt.suptitle("First 100 faces")
+		plt.suptitle(title)
 		plt.show()
 
-		# for i in range(10):
-		# 	for j in range(10):
-		# 		im = X[i].reshape(32, 32)
-		# 		ax[i, j].imshow(im)
+	def test(self, X_norm, X_rec):
 
+		self.plotFaces(X_norm[:100])
+		self.plotFaces(X_rec[:100])
+
+	def plot3D(self, X, sel, colors):
+
+		fig = plt.figure(1)
+		ax = fig.add_subplot(111, projection="3d")
+
+		ax.scatter(X[sel, 0], X[sel, 1], X[sel, 2], s=25, c=colors)
+		plt.title("Pixel dataset plotted in 3D.Colors shows centroids memberships.")
 		plt.show()
 
+	def plot2D(self, X, idx, K):
 
+		palette = colors.hsv_to_rgb( np.column_stack([ np.linspace(0, 1, K+1), np.ones(((K+1), 2) ) ]) )
+		color = np.array([palette[int(i)] for i in idx])
+
+		plt.scatter(X[:,0], X[:,1], s=15, facecolors="none", edgecolor=color)
+		plt.title("Pixels dataset plotted in 2D, using PCA for dimensionality reduction")
+		plt.show()
 
 
 def main():
@@ -175,42 +206,103 @@ def main():
 	path_faces = current_path + "/ex7faces.mat"
 	pca = PCA(path_data, path_faces)
 
-	# pca.plotData(pca.getX())
+	# # pca.plotData(pca.getX())
+
+	# """
+	# # ==================== Part 2: Prinipal Components Analysis ===============
+	# """
+
+	# X_norm, mu, sigma = pca.featureNormalize(pca.getX())
+	# u, s, _ = pca.pca(X_norm)
+	# # pca.plotData(pca.getX(), (0.5, 6.5), (2, 8), mu, s, u, 1)
+	# # pca.plotData(pca.getX(), mu, s, u, 1)
+
+	# """
+	# # =================== Part 3: Dimension Reduction =========================
+	# # Implement projection step to map the data onto the first k eigenvectors.
+	# # Then plot the data in tthis reduced dimension spac. This will show what data
+	# # looks like, when using inly correspoding eigenvectors to reconstruct it.
+	# """
+
+	# # pca.plotData(X_norm, (-4, 3), (-4, 3))
+	# K = 1
+	# Z = pca.projectData(X_norm ,u, K)
+	# X_rec = pca.recoverData(Z, u, K)
+	# # pca.plotProjection(X_norm, X_rec)
+
+	# """
+	# # =================== Part 4: Loading and Visualizing FAce Data ===========
+	# """
+
+	# # print(pca.Xf.shape)
+	# # pca.plotFaces(pca.getXFaces())
+
+	# """
+	# # =================== Part 5: PCA on Face Data: Eigenfaces ================
+	# # Run PCA and visualise the eigenvectirs which are in thhis case eigenfaces
+	# # We display first 36 eigenfaces
+	# """
+
+	# X_norm, mu, sigma = pca.featureNormalize(pca.getXFaces())
+
+	# U, S, VH = pca.pca(X_norm)
+	# print(U.shape)
+	# # pca.plotFaces(-U.T, 6, "Principal components on the face dataset")
+
+	# """
+	# # =================== Part 6: Dimension Reduction for Faces ===============
+	# # Project images to the eigen space using the top k eigenvectors
+	# """
+
+	# K = 100
+	# Z = pca.projectData(X_norm, U, K)
+
+	# """
+	# # =================== Part 7: Visualization of Faces after PCA Dimension Reduction 
+	# # Project images to the eigen space using the top k eigenvectors 
+	# """
+
+	# X_rec = pca.recoverData(Z, U, K)
+	# pca.test(X_norm, X_rec)
 
 	"""
-	# ==================== Part 2: Prinipal Components Analysis ===============
+	# =================== Part 8(a): PCA For Visualisation =======================
+	# One useful apllication of PCA is to use it to visualize high-dimensional
+	# data. We'll run K-Means on 3-dimensional pixels colors of an image. 
+	# Firstly visualize this output in 3D, and the apply PCA to obtain a 
+	# visualization in 2D.
 	"""
 
-	X_norm, mu, sigma = pca.featureNormalize(pca.getX())
-	u, s, _ = pca.pca(X_norm)
-	print(s)
-	print(u)
-	# pca.plotData(pca.getX(), (0.5, 6.5), (2, 8), mu, s, u, 1)
-	# pca.plotData(pca.getX(), mu, s, u, 1)
+	path_data2 = current_path + "/ex7data2.mat"
+	path_image = current_path + "/bird_small.png"
+	path_image_mat = current_path + "/bird_small.mat"
+
+	kmeans = kM.KMeans(path_data2, path_image)
+	X = kmeans.getX()
+
+	K = 16
+	max_iters = 10
+	initial_centroids = kmeans.KmeansInitCentroids(X, K)
+	centroids, idx = kmeans.runkMeans(X, initial_centroids, max_iters)
+
+	sel = np.floor(np.random.rand(1000, 1) * X.shape[0]).astype(int).flatten()
+
+	palette = colors.hsv_to_rgb( np.column_stack([ np.linspace(0, 1, K+1), np.ones(((K+1), 2) ) ]) )
+	color = palette[idx[sel]]
+
+	pca.plot3D(X, sel, color)
 
 	"""
-	# =================== Part 3: Dimension Reduction =========================
-	# Implement projection step to map the data onto the first k eigenvectors.
-	# Then plot the data in tthis reduced dimension spac. This will show what data
-	# looks like, when using inly correspoding eigenvectors to reconstruct it.
+	# =================== Part 8(b): PCA for Visualization ====================
 	"""
 
-	# pca.plotData(X_norm, (-4, 3), (-4, 3))
-	K = 1
-	Z = pca.projectData(X_norm ,u, K)
-	X_rec = pca.recoverData(Z, u, K)
-	# pca.plotProjection(X_norm, X_rec)
+	X_norm, mu, sigma = pca.featureNormalize(X)
 
-	"""
-	# =================== Part 4: Loading and Visualizing FAce Data ===========
-	"""
+	U, S, VH = pca.pca(X_norm)
 
-	print(pca.Xf.shape)
-	pca.plotFaces(pca.getXFaces())
+	Z = pca.projectData(X_norm, U, 2)
 
-
-
-
+	pca.plot2D(Z[sel, :], idx[sel], K)
 
 
 if __name__ == "__main__":
